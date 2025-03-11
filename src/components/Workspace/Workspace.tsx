422736bd33ed3e07b60c8af221219520c795a204
DurgePratik <iitianpratik2022@gmail.com>
2025-03-12 00:51:37 +0530

    your mesage

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -4,26 +4,27 @@ import Split from 'react-split';
 import ProblemDescription from './ProblemDescription/ProblemDescription';
 import Playground from './Playground/Playground';
 import type { Problem } from '@/utils/types/problems';
-import Confetti from "react-confetti"
+import Confetti from "react-confetti";
 import useWindowSize from '@/hooks/useWindowSize';
 
 type WorkspaceProps = {
     problem: Problem
 };
 
-const Workspace:React.FC<WorkspaceProps> = ({problem}) => {
-   const {width,height} = useWindowSize();
-   const [success,setSuccess] = useState( false);
-   const [solved,setSolved] =useState( false);
+const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
+    const { width, height } = useWindowSize();
+    const [success, setSuccess] = useState(false);
+    const [solved, setSolved] = useState(false);
 
     return (
-    <Split className='split'>
-        <ProblemDescription problem={problem} _solved ={solved} />
-        <div> 
-            <Playground problem={problem}  setSuccess ={setSuccess} setSolved={setSolved} />
-            {success && <Confetti  gravity={0.3} tweenDuration={4000} width={width} height={height} />}
+        <Split className='split'>
+            <ProblemDescription problem={problem} solved={solved} />
+            <div> 
+                <Playground problem={problem} setSuccess={setSuccess} setSolved={setSolved} />
+                {success && <Confetti gravity={0.3} tweenDuration={4000} width={width} height={height} />}
             </div>
-    </Split>
+        </Split>
     );
 }
+
 export default Workspace;
